5f49b1df7aa0a16588fbee6ee9751eec20af5870
ATAN <natanahelatankeu@gmail.com>
2024-11-25 19:44:50 +0100

    insert 'project' in the home route of the app.py module

--- Scripts/app.py
+++ Scripts/app.py
@@ -1,11 +1,13 @@
 from flask import Flask, jsonify
 
+
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/project')
 def home():
     return jsonify(message="Hello level 400 FET, Quality Assurance!")
 
+
 if __name__ == '__main__':
-    app.run(debug=True);
+    app.run(debug=True)
